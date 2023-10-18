90bbad322921c359608fcde326584b0cad2ec8a9
a.krasnov <wonsarc@icloud.com>
2023-10-18 15:02:31 +0300

    add gradient

--- ImageFeed/ImageList/ImagesListCell.swift
+++ ImageFeed/ImageList/ImagesListCell.swift
@@ -14,34 +14,43 @@ final class ImagesListCell: UITableViewCell {
     @IBOutlet private weak var dateLabel: UILabel!
     @IBOutlet private weak var gradientView: UIView!
     
+    // MARK: - Private Properties
+    private var isGradient: Bool = false
+    
     // MARK: - Public Properties
     static let reuseIdentifier = "ImagesListCell"
     
     // MARK: - Public Methods
     func configureCell(image: UIImage?, date: String, isLiked: Bool) {
-       gradientView.backgroundColor = .clear
-       cellView.layer.cornerRadius = 16
-       cellView.layer.masksToBounds = true
-       
-       cellView.image = image
-       dateLabel.text = date
-       let likedButtonImage = isLiked ? "Active" : "No Active"
-       likeButton.setImage(UIImage(named: likedButtonImage), for: .normal)
-   }
-    
+        cellView.layer.cornerRadius = 16
+        cellView.layer.masksToBounds = true
+        cellView.image = image
+        dateLabel.text = date
+        let likedButtonImage = isLiked ? "Active" : "No Active"
+        likeButton.setImage(UIImage(named: likedButtonImage), for: .normal)
+    }
     
     // MARK: - Private Methods
-    func configGradientView() {
+    private func configGradientView() {
         gradientView.backgroundColor = .clear
-        let gradientLayer:CAGradientLayer = CAGradientLayer()
+        let gradientLayer: CAGradientLayer = CAGradientLayer()
         gradientLayer.frame = gradientView.bounds
         gradientLayer.colors = [
             UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0.0).cgColor,
             UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0.2).cgColor
         ]
-        //        gradientLayer.startPoint = CGPointMake(0.5, 0.0)
-        //        gradientLayer.endPoint = CGPointMake(0.5, 1.0)
-        gradientLayer.locations = [0.0, 1.0]
-        gradientView.layer.addSublayer(gradientLayer)
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientView.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    // MARK: - Override Methods
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if !isGradient {
+            configGradientView()
+            isGradient = true
+        }
     }
 }
