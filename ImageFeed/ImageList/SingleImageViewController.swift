0be9e33d25d5cc6941a3164d4abcaa368f867b61
a.krasnov <wonsarc@icloud.com>
2024-01-07 22:56:03 +0300

    refactoring segue

--- ImageFeed/ImageList/SingleImageViewController.swift
+++ ImageFeed/ImageList/SingleImageViewController.swift
@@ -5,4 +5,27 @@
 //  Created by Artem Krasnov on 27.11.2023.
 //
 
-import Foundation
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    // MARK: - Public Properties
+    var image: UIImage! {
+        didSet {
+            guard isViewLoaded else { return }
+            photoSingleImageView.image = image
+        }
+    }
+    
+    // MARK: - IB Outlets
+    @IBOutlet private weak var photoSingleImageView: UIImageView!
+    
+    // MARK: - IB Action
+    @IBAction func didTapBackButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        photoSingleImageView.image = image
+    }
+}
