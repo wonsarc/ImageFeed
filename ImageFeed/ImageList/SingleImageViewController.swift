756a88372ca16cbcf0cf87a4bc2f77fd9e5ad102
a.krasnov <wonsarc@icloud.com>
2024-01-09 22:55:58 +0300

    refactoring ProfileView

--- ImageFeed/ImageList/SingleImageViewController.swift
+++ ImageFeed/ImageList/SingleImageViewController.swift
@@ -13,19 +13,58 @@ final class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             photoSingleImageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
     // MARK: - IB Outlets
     @IBOutlet private weak var photoSingleImageView: UIImageView!
     
+    @IBOutlet private weak var scrollView: UIScrollView!
+    
+    @IBOutlet private weak var sharedButton: UIButton!
+    
     // MARK: - IB Action
     @IBAction func didTapBackButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func didTapShareButton(_ sender: Any) {
+        present(UIActivityViewController(activityItems: [image as Any], applicationActivities: nil),
+                animated: true)
+    }
+    
+    // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         photoSingleImageView.image = image
+        rescaleAndCenterImageInScrollView(image: image)
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+    }
+    
+    // MARK: - Private func
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
+
+// MARK: - UIScrollViewDelegate
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        photoSingleImageView
     }
 }
--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -8,28 +8,112 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    // MARK: - IB Outlets
-    @IBOutlet weak private var logoImageView: UIImageView!
-    @IBOutlet weak private var nameLabel: UILabel!
-    @IBOutlet weak private var loginLabel: UILabel!
-    @IBOutlet weak private var descriptionLabel: UILabel!
-    @IBOutlet weak private var logoutButton: UIButton!
-    
-    // MARK: - IB Action
-    @IBAction private func didTapLogoutButton(_ sender: Any) {
-    }
-    
     // MARK: - View Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
-        lel()
+        let logoImageView = addLogoImageView(name: "profilePhoto")
+        let nameLabel = addNameLabel(text: "Екатерина Новикова")
+        let loginLabel = addLoginLabel(text: "@ekaterina_nov")
+        let logoutButton = addLogoutButton()
+        let descriptionLabel = addDescriptionLabel(text: "Hello, world!")
+        
+        setConstraints(
+            logoImageView: logoImageView,
+            nameLabel: nameLabel,
+            loginLabel: loginLabel,
+            descriptionLabel: descriptionLabel,
+            logoutButton: logoutButton
+        )
     }
     
-    private func lel() {
-        nameLabel.text = "Екатерина Новикова"
-        loginLabel.text = "@ekaterina_nov"
-        descriptionLabel.text = "Hello, world!"
+    // MARK: - Private Func
+    private func addLogoImageView(name: String) -> UIImageView {
+        let logoImageView = UIImageView()
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
         logoImageView.layer.cornerRadius = 16
-        logoImageView.image = UIImage(named: "profilePhoto")
+        logoImageView.image = UIImage(named: name)
+        view.addSubview(logoImageView)
+        return logoImageView
+    }
+    
+    private func addNameLabel(text: String) -> UILabel {
+        let nameLabel = UILabel()
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.text = text
+        nameLabel.textColor = .ypWhite
+        nameLabel.font = .boldSystemFont(ofSize: 23)
+        view.addSubview(nameLabel)
+        return nameLabel
+    }
+    
+    private func addLoginLabel(text: String) -> UILabel {
+        let loginLabel = UILabel()
+        loginLabel.translatesAutoresizingMaskIntoConstraints = false
+        loginLabel.text = text
+        loginLabel.textColor = .ypGray
+        loginLabel.font = .systemFont(ofSize: 13)
+        view.addSubview(loginLabel)
+        return loginLabel
+    }
+    
+    private func addDescriptionLabel(text: String) -> UILabel {
+        let descriptionLabel = UILabel()
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.text = text
+        descriptionLabel.textColor = .ypWhite
+        descriptionLabel.font = .systemFont(ofSize: 13)
+        view.addSubview(descriptionLabel)
+        return descriptionLabel
+    }
+    
+    private func addLogoutButton() -> UIButton {
+        let logoutButton = UIButton.systemButton(
+            with: UIImage(named: "LogOut") ?? UIImage(),
+            target: self,
+            action: #selector(didTapLogoutButton)
+        )
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        logoutButton.tintColor = .ypRed
+        view.addSubview(logoutButton)
+        return logoutButton
+    }
+    
+    private func setConstraints(
+        logoImageView: UIImageView,
+        nameLabel: UILabel,
+        loginLabel: UILabel,
+        descriptionLabel: UILabel,
+        logoutButton: UIButton
+    ) {
+        NSLayoutConstraint.activate([
+            logoImageView.widthAnchor.constraint(equalToConstant: 70),
+            logoImageView.heightAnchor.constraint(equalToConstant: 70),
+            logoImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 52),
+            
+            logoutButton.widthAnchor.constraint(equalToConstant: 48),
+            logoutButton.heightAnchor.constraint(equalToConstant: 48),
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -18),
+            logoutButton.centerYAnchor.constraint(equalTo: logoImageView.centerYAnchor),
+            
+            nameLabel.widthAnchor.constraint(equalToConstant: 700),
+            nameLabel.heightAnchor.constraint(equalToConstant: 18),
+            nameLabel.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 8),
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            loginLabel.widthAnchor.constraint(equalToConstant: 400),
+            loginLabel.heightAnchor.constraint(equalToConstant: 18),
+            loginLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            loginLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            descriptionLabel.widthAnchor.constraint(equalToConstant: 400),
+            descriptionLabel.heightAnchor.constraint(equalToConstant: 18),
+            descriptionLabel.topAnchor.constraint(equalTo: loginLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
+    }
+    
+    @objc
+    private func didTapLogoutButton() {
     }
 }
